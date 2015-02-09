08e8b6a87f2d9614bf19e2b6b8229eff05c13499
ANUPAM GANGOTIA <agangotia@gmail.com>
2015-02-09 00:35:44 -0500

    initial project version

---
+++ src/log4jIntro/Testlog4j.java
@@ -0,0 +1,15 @@
+package log4jIntro;
+
+import org.apache.log4j.Logger;
+
+public class Testlog4j {
+	
+	final static Logger logger = Logger.getLogger(Testlog4j.class);
+	
+	
+	public static void main(String[] args) {
+		 
+		logger.info("This is info : ");
+	}
+
+}
